2453cab5189e79e4f9c297b55af5925e4f64eef0
Taliga <taligamatus@gmail.com>
2024-01-07 13:38:52 +0100

    Homework 6

--- uu_shoplisttaliga_maing01-hi/src/core/ShoppingList/infoBar.js
+++ uu_shoplisttaliga_maing01-hi/src/core/ShoppingList/infoBar.js
@@ -1,11 +1,30 @@
 import { useDataController, useDataSelection, Lsi } from "uu5g05";
-import Uu5Elements from "uu5g05-elements";
+import Uu5Elements, { Grid } from "uu5g05-elements";
+import { PieChart } from "uu5chartsg01";
 import Config from "../config/config";
 import importLsi from "../../lsi/import-lsi.js";
 
 function InfoBar({ toggleShowNotSelected, showNotSelected }) {
   let { data } = useDataController();
   let { selectedData, isDisplayedSelected } = useDataSelection();
+
+  const pieChartData = [
+    {
+      value: selectedData.length,
+      label: "Completed",
+    },
+    {
+      value: data.length - selectedData.length,
+      label: "Not completed",
+    },
+  ];
+
+  const pieChartSerieList = [
+    {
+      valueKey: "value",
+      labelKey: "label",
+    },
+  ];
 
   return (
     <div>
@@ -22,13 +41,16 @@ function InfoBar({ toggleShowNotSelected, showNotSelected }) {
         )}
       </Uu5Elements.Button>
 
-      {/*       <Uu5Elements.Block className={Config.Css.css({ width: "100%", margin: 10 })}>
-        {isDisplayedSelected
-          ? ` Displaying ${data.length} selected item(s).`
-          : ` ${(<Lsi import={importLsi} path={["DisplayedItems", "completedFirst"]} />)} + ${
-              selectedData.length
-            } item(s) out of ${data.length} total.`}
-      </Uu5Elements.Block> */}
+      {
+        <Uu5Elements.Block className={Config.Css.css({ width: "100%", margin: 10 })}>
+          {isDisplayedSelected
+            ? ` Displaying ${data.length} selected item(s).`
+            : ` ${selectedData.length} item(s) completed out of ${data.length} total.`}
+          <Grid>
+            <PieChart data={pieChartData} seriesList={pieChartSerieList} />
+          </Grid>
+        </Uu5Elements.Block>
+      }
     </div>
   );
 }
